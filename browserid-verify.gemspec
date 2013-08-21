c228e26e30777205e2289dc99529caee824a3eec
Andrew Chilton <andychilton@gmail.com>
2013-08-21 18:02:08 +1200

    Commit something that is working

---
+++ browserid-verify.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'browserid-verify'
+  s.version     = '0.1.0'
+  s.date        = '2013-08-21'
+  s.summary     = "A BrowserID Verifier."
+  s.description = "Verify assertions either remotely or locally (remote only for the moment)."
+  s.authors     = ["Andrew Chilton"]
+  s.email       = 'chilts@mozilla.com'
+  s.files       = ["lib/browserid-verify.rb"]
+  s.homepage    = 'http://rubygems.org/gems/browserid-verify'
+  s.license     = 'MIT'
+end
---
+++ test/failure.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require "./lib/browserid-verify.rb"
+
+class TC_Verify < Test::Unit::TestCase
+  def test_simple_1
+    v = Verify.new('remote', 'http://localhost')
+    data = v.verify('invalid assertion')
+    assert_equal(data['status'], 'failure')
+    assert_equal(data['reason'], 'no certificates provided')
+  end
+
+  def test_simple_2
+    data = verify_remotely('http://localhost', 'invalid assertion')
+    assert_equal(data['status'], 'failure')
+    assert_equal(data['reason'], 'no certificates provided')
+  end
+end
